b9ec5dfc185016a5515e8a0f205d6687108ccb49
Breyelle Bradley <breyellebradley@gmail.compwdgit config --global user.email breyellebradley@gmail.com>
2025-06-10 08:34:50 -0500

    added sum mo

---
+++ NorthwindTraders/src/main/java/App.java
@@ -0,0 +1,53 @@
+public class App {
+    public static void main(String[] args) {
+        try {
+
+            // 1. open a connection to the database
+            // use the database URL to point to the correct database
+
+            //this is like opening MySQL workbench and clicking localhost
+            Connection connection;
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "yearup");
+
+
+            // create statement
+            // the statement is tied to the open connection
+
+            //like me opening a new query window
+            Statement statement = connection.createStatement();
+
+            // define your query
+
+            //like me typing the query in the new query windows
+            String query = "SELECT ProductName FROM Products";
+
+            // 2. Execute your query
+
+            //this is like me clicking the lightning bolt
+            ResultSet results = statement.executeQuery(query);
+
+            // process the results
+            //this is a way to view the result set but java doesnt have a spreadsheet view for us
+            while (results.next()) {
+                String title = results.getString("Title");
+                System.out.println(title);
+            }
+
+            // 3. Close the connection
+
+            //closing mysql workbench
+            connection.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            //closing mysql workbench
+            connection.close();
+        }
+
+
+    }
+
+
+}
+
